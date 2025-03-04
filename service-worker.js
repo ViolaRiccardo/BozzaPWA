70d29ad9d8c47b6673d4640a681e145fbdbebc36
Riccardo Viola <riccardo.viola@atom.it>
2025-03-04 12:27:23 +0100

    prova aggiornamento funzionale app

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,28 @@
-self.addEventListener('install', event => {
+const CACHE_NAME = 'my-pwa-cache-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/styles.css'
+];
+
+self.addEventListener('install', function(event) {
     event.waitUntil(
-        caches.open('pwa-cache').then(cache => {
-            return cache.addAll(['index.html']);
-        })
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                console.log('Cache aperta');
+                return cache.addAll(urlsToCache);
+            })
     );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', function(event) {
     event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
+        caches.match(event.request)
+            .then(function(response) {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
     );
 });
